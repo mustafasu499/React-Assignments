95c6346eced083184e33a3fe24eba238ca78c0ca
mustafasu499 <mhstafasu499@gmail.com>
2024-06-27 22:54:42 +0500

    Assignment-5 Done

---
+++ Assignment-5/src/demo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import ReactDOM from "react-dom/client";
+
+const Demo = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello React</h1>
+        <p>React is a JavaScript library for building user interfaces.</p>
+        <img src="src/assets/carimage.png" alt="" />
+        <br />
+        <input type="text" />
+        <button>submit</button>
+      </div>
+    </>
+  );
+};
+
+export default Demo;
---
+++ Assignment-5/src/main.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Demo from "./demo";
+ReactDOM.createRoot(document.getElementById("root")).render(<Demo />);
